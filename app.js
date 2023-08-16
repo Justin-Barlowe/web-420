48fd4120cc882ca7965a7282741cb8888505cbe5
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2023-08-16 12:06:18 -0400

    Updating assignment 1

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
+//require statements
 const express = require('express');
 const http = require('http');
 const swaggerUi = require('swagger-ui-express');
 const swaggerjsdoc = require('swagger-jsdoc');
 const mongoose = require('mongoose');
 
+//App
 const app = express();
 
+//Port assignment
 const port = process.env.PORT || 3000;
 
+//Json
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+//options
 const options = {
     definition: {
         openapi: '3.0.0',
@@ -23,15 +28,18 @@ const options = {
     apis: ['./routes/*.js'],
 };
 
-const openapiSpecification = swaggerjsdoc(options); // Define this before using it
+//openapiSpecification
+const openapiSpecification = swaggerjsdoc(options); 
 
+//app 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
+//get request
 app.get('/', (req, res) => {
     res.redirect('/api-docs');
   });
   
-
+//http server
 http.createServer(app).listen(port, function() {
     console.log(`Application started and listening on port: ${port}`);
 });
